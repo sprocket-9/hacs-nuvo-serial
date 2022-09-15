96e1adb59e4bbd9331877620c18d7e1cad180283
sprocket-9 <sprocketnumber9@gmail.com>
2022-09-15 18:44:05 +0100

    Make integration services device aware.
    Add configure_time integration service.
    
    Improve services.yaml by using selectors.

--- custom_components/nuvo_serial/__init__.py
+++ custom_components/nuvo_serial/__init__.py
@@ -2,12 +2,13 @@
 import logging
 
 from nuvo_serial import get_nuvo_async
+from nuvo_serial.grand_concerto_essentia_g import NuvoAsync
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_DEVICE_ID, CONF_PORT, CONF_TYPE, Platform
 from homeassistant.core import HomeAssistant, ServiceCall
-from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.exceptions import ConfigEntryNotReady, HomeAssistantError
 from homeassistant.helpers import config_validation as cv, device_registry as dr
 from homeassistant.helpers.typing import ConfigType
 
@@ -72,21 +73,48 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    async def page_on(call: ServiceCall) -> None:
+    def nuvo_connection(func):
+        """Decorate a service call func with the nuvo connection associated with a service call device_id.
+
+        If there are multiple nuvo_serial integrations installed this ensures the
+        service call is made to the correct system.
+        """
+
+        async def get_nuvo_object_from_device_id(call: ServiceCall):
+            nuvo_object = None
+            device_id = call.data[ATTR_DEVICE_ID]
+            if device := device_registry.async_get(device_id):
+                for config_entry_id in device.config_entries:
+                    if data := hass.data[DOMAIN].get(config_entry_id):
+                        if nuvo_object := data.get(NUVO_OBJECT):
+                            break
+            if nuvo_object is None:
+                raise HomeAssistantError(
+                    f"Nuvo connection not found for device_id: {call.data[ATTR_DEVICE_ID]}"
+                )
+            await func(call, nuvo_object)
+
+        return get_nuvo_object_from_device_id
+
+    @nuvo_connection
+    async def page_on(call: ServiceCall, connection: NuvoAsync) -> None:
         """Service call to turn paging on."""
-        await nuvo.set_page(True)
+        await connection.set_page(True)
 
-    async def page_off(call: ServiceCall) -> None:
+    @nuvo_connection
+    async def page_off(call: ServiceCall, connection: NuvoAsync) -> None:
         """Service call to turn paging off."""
-        await nuvo.set_page(False)
+        await connection.set_page(False)
 
-    async def all_off(call: ServiceCall) -> None:
+    @nuvo_connection
+    async def all_off(call: ServiceCall, connection: NuvoAsync) -> None:
         """Service call to turn all zones off."""
-        await nuvo.all_off()
+        await connection.all_off()
 
-    async def configure_time(call: ServiceCall) -> None:
-        """Service call to set the system timeturn all zones off."""
-        await nuvo.configure_time(call.data[SERVICE_ATTR_DATETIME])
+    @nuvo_connection
+    async def configure_time(call: ServiceCall, connection: NuvoAsync) -> None:
+        """Service call to set the system time."""
+        await connection.configure_time(call.data[SERVICE_ATTR_DATETIME])
 
     hass.services.async_register(DOMAIN, SERVICE_PAGE_ON, page_on, schema=DEVICE_SCHEMA)
 
@@ -94,7 +122,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         DOMAIN, SERVICE_PAGE_OFF, page_off, schema=DEVICE_SCHEMA
     )
 
-    hass.services.async_register(DOMAIN, SERVICE_ALL_OFF, all_off, schema=None)
+    hass.services.async_register(DOMAIN, SERVICE_ALL_OFF, all_off, schema=DEVICE_SCHEMA)
 
     hass.services.async_register(
         DOMAIN, SERVICE_CONFIGURE_TIME, configure_time, schema=CONFIGURE_TIME_SCHEMA
